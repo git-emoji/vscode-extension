230aff6a1d9359448a33b06a30277af32287fe75
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2023-05-05 22:33:30 +0100

    🔨 Add method to detect index of whitespace following first word
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,16 @@
+const _FIRST_NON_WHITESPACE_SEQUENCE = /^\s*[^\s]+/u;
+
+/**
+ * @returns Index of the first whitespace character following the first word; otherwise, returns -1.
+ */
+export function getFirstWhitespaceAfterFirstWord(s: string): number {
+    const match = s.match(_FIRST_NON_WHITESPACE_SEQUENCE);
+    if (!match) {
+        return -1;
+    }
+    return match[0].length;
+}
+
 export function normalizeWord(word: string) {
     return word.trim().toLowerCase();
 }
