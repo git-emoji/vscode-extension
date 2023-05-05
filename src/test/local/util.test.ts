5ec0de566207815d93ecbb658b811363e9b8cf06
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2023-05-05 22:34:07 +0100

    ⚗️ Add tests for `getFirstWhitespaceAfterFirstWord`
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- src/test/local/util.test.ts
+++ src/test/local/util.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import { normalizeWord } from '../../util';
+import { getFirstWhitespaceAfterFirstWord, normalizeWord } from '../../util';
 
 const se: typeof assert.strictEqual = assert.strictEqual;
 
@@ -13,4 +13,17 @@ test('normalizeWord', () => {
     se(sut('  wrapped-by-whitespace  '), 'wrapped-by-whitespace');
     se(sut('mixed-CASEs'), 'mixed-cases');
     se(sut(' mixed-CASEs-with-whitespace '), 'mixed-cases-with-whitespace');
+});
+
+test('getFirstWhitespaceAfterFirstWord', () => {
+    const sut = getFirstWhitespaceAfterFirstWord;
+    se(sut(''), -1, 'empty');
+    se(sut(' '), -1, 'whitespace');
+    se(sut('     '), -1, 'whitespace+');
+    se(sut('  \t '), -1, 'whitespace+ (including tabs)');
+    se(sut('something'), 9, 'word');
+    se(sut(' something'), 10, 'whitespace+word 1');
+    se(sut('\tsomething'), 10, 'whitespace+word 2');
+    se(sut(' \t something'), 12, 'whitespace+word 3');
+    se(sut(' something '), 10, 'whitespace+word+whitespace');
 });
